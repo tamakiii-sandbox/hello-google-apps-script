ee58488693a9591e6ef3f7d72428426f9da2ebb8
tamakiii <tamakiii@users.noreply.github.com>
2020-06-09 00:21:22 +0900

    fm~

--- clasp.mk
+++ clasp.mk
@@ -1,4 +1,4 @@
-.PHONY: help install dependencies clean
+.PHONY: help install dependencies login login-status setting create list open clean
 
 help:
 	@cat $(firstword $(MAKEFILE_LIST))
@@ -16,11 +16,17 @@ login:
 login-status:
 	npx clasp login --status
 
+setting:
+	npx clasp setting
+
 create:
 	npx clasp create --type standalone --rootDir src
 
 list:
 	npx clasp list
 
+open:
+	npx clasp open
+
 clean:
-	#
+	rm -rf .clasp.json
